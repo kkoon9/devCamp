ab165d083182fc62660de16914faf4d99c458964
kkoon9 <rndrnjs2003@gmail.com>
2020-10-17 11:19:16 +0900

    Add morgan for middleware

--- src/server.js
+++ src/server.js
@@ -5,9 +5,15 @@ dotenv.config({ path: './config/config.env' });
 
 // Route files 
 const bootcamps = require('./api/bootcamps');
+const morgan = require("morgan");
 
+// logger files
 const app = express();
 
+// Dev logging middleware 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 // Mount apis
 app.use('/api/v1/bootcamps', bootcamps);
 
